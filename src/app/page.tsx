965f8c0774d9654d641c5cc9cf0f3908f03c50fe
Fede <spagnolofederico3@gmail.com>
2024-12-06 01:17:18 -0300

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import HeroSection from "@/components/home/HeroSection";
 
 export const metadata = {
- title: 'Inicio',
- description: 'Inicio',
+  title: "Inicio",
+  description: "Inicio",
 };
 
 export default function Home() {
   return (
     <>
-    HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-      <HeroSection/>
+      HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+      <HeroSection />
     </>
   );
 }
